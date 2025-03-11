880b3b0fbdd92e5a267f2427ca43da7dad770dda
Herick Kumata <herickyk@gmail.com>
2025-03-11 11:50:56 -0300

    mudando o comentário no balanceControllers

--- src/controllers/balanceControllers.js
+++ src/controllers/balanceControllers.js
@@ -5,17 +5,17 @@ const getBalance = (req, res) => {
   //extraindo o valor de account_id da query String da requisição;
   const { account_id } = req.query;
 
-  //importando o id de accounts[id]
+  //Buscando a conta pelo account_id
   const account = accountService.getAccount(account_id);
 
-  //Resposta de 404 0 caso não exista
+  //Verificação se a conta existe
   if (!account) {
     return res.status(404).json(0);
   }
 
   //Caso Exista: 200 valor do balance
-  res.status(200).json(account.balance);
+  res.status(200).json({ balance: account.balance});
 };
 
 //Importação do módulo de getBalance
-module.exports = { getBalance };
+module.exports = { getBalance }; 
